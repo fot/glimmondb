7bbc6e2f844afcc5dc3ce357f88e0ddb02d69d20
matthewdahmer <matthew.dahmer@gmail.com>
2015-12-01 14:53:00 -0500

    Check before current date, add comments

--- glimmondb_tests.py
+++ glimmondb_tests.py
@@ -6,10 +6,12 @@ from shutil import copytree, rmtree, copy
 
 from Chandra.Time import DateTime
 
+# Run this to test:
+# py.test -s glimmondb_tests.py
+
 # Set this for now, this should not be necessary in production
 environ["SKA_DATA"] = "/home/mdahmer/AXAFAUTO/"
 environ["TDBDATA"] = "/home/mdahmer/AXAFAUTO/TDB_Archive/"
-
 
 try:
     rmtree('./testing_data')
@@ -25,24 +27,22 @@ except:
     print 'Could not delete old log'
 import glimmondb
 
-print('Regenerated testing folder at: ./testing_data')
+print('\nRegenerated testing folder at: ./testing_data')
 print('Using G_LIMMON Archive at: {}'.format(glimmondb.DBDIR))
 print('Using TDB Archive at: {}'.format(glimmondb.TDBDIR))
-
-# mknod('./testing_data/DB_Commit.log')
 
 def querydatabase(glimmondbfile, datecheckbefore):
     db = connect(glimmondbfile)
     cursor = db.cursor()
 
     cursor.execute('''SELECT msid, setkey, datesec, date, modversion, mlmenable, mlmtol, 
-                      default_set, mlimsw, caution_high, caution_low, warning_high, warning_low, 
+                      default_set, mlimsw, caution_high, caution_low, warning_high, warning_low,
                       switchstate FROM limits WHERE datesec < ?''', (datecheckbefore,))
     all_limits = cursor.fetchall()
 
-    cursor.execute('''SELECT msid, setkey, datesec, date, modversion, mlmenable, mlmtol, default_set,
-                      mlimsw, expst, switchstate FROM expected_states WHERE datesec < ?''',
-                      (datecheckbefore,))
+    cursor.execute('''SELECT msid, setkey, datesec, date, modversion, mlmenable, mlmtol,
+                      default_set, mlimsw, expst, switchstate FROM expected_states WHERE 
+                      datesec < ?''', (datecheckbefore,))
     all_states = cursor.fetchall()
 
     cursor.execute('''SELECT version, datesec, date FROM versions WHERE datesec < ?''',
@@ -78,10 +78,9 @@ def saveoutputs(all_limits, all_states, all_versions, prestr='new'):
 
 
 def test_function():
-    datecheckbefore = DateTime('2015:321:00:00:00').secs
-    oldlimithash = '92b6b2069bb64b0c784d7755370f7c0dc6df9c8b6da0e817bc77fdfaa8225d64'
-    oldstatehash = 'c4f5ee8ad7d7a11054196f9c91ebc3751d799816d60cf0667c3d9f2777d7f8db'
-    oldversionhash = '94eedd6f4fa203daadb62746c3dbc85e6f154abcd448c69c58a7f7e2f8309a31'
+    # Insert a date in the DateTime function below to compare the new and old databases before a
+    # specific date.
+    datecheckbefore = DateTime().secs
 
     glimmondb.recreate_db(glimmondbfile='glimmondb_testing.sqlite3')
     copy('./testing_data/glimmondb_testing.sqlite3', './glimmondb_testing_backup.sqlite3')
@@ -95,11 +94,17 @@ def test_function():
     saveoutputs(new_all_limits, new_all_states, new_all_versions, prestr='new')
     saveoutputs(old_all_limits, old_all_states, old_all_versions, prestr='new')
 
-    newlimithash, newstatehash, newversionhash = gethashes(new_all_limits, new_all_states, new_all_versions)
-    oldlimithash, oldstatehash, oldversionhash = gethashes(old_all_limits, old_all_states, old_all_versions)
+    newlimithash, newstatehash, newversionhash = gethashes(new_all_limits, new_all_states, 
+                                                           new_all_versions)
+    oldlimithash, oldstatehash, oldversionhash = gethashes(old_all_limits, old_all_states,
+                                                           old_all_versions)
 
-    print('newlimithash = {}\nnewstatehash = {}\nnewversionhash = {}'.format(newlimithash, newstatehash, newversionhash))
-    print('oldlimithash = {}\noldstatehash = {}\noldversionhash = {}'.format(oldlimithash, oldstatehash, oldversionhash))
+    print('newlimithash = {}\nnewstatehash = {}\nnewversionhash = {}'.format(newlimithash,
+                                                                             newstatehash,
+                                                                             newversionhash))
+    print('oldlimithash = {}\noldstatehash = {}\noldversionhash = {}'.format(oldlimithash,
+                                                                             oldstatehash,
+                                                                             oldversionhash))
 
     assert oldlimithash == newlimithash
     assert oldstatehash == newstatehash
