02e99a8b53bec65ebc0c5400161d30935621a2a2
matthewdahmer <github@matthewdahmer.com>
2025-12-20 12:09:07 -0500

    move logged actions to separate fileSplit update/logging code into glimmondb.update, add NullHandler, and keep update APIs via lazy import.

--- glimmondb/__init__.py
+++ glimmondb/__init__.py
@@ -1,2 +1,21 @@
-from .glimmondb import get_tdb, read_glimmon, create_db, recreate_db, merge_new_glimmon_to_db, DBDIR, TDBDIR
+from .glimmondb import get_tdb, read_glimmon, DBDIR, TDBDIR
 from .version import __version__
+
+__all__ = [
+    "get_tdb",
+    "read_glimmon",
+    "DBDIR",
+    "TDBDIR",
+    "create_db",
+    "recreate_db",
+    "merge_new_glimmon_to_db",
+    "compute_fingerprints",
+    "__version__",
+]
+
+
+def __getattr__(name):
+    if name in {"create_db", "recreate_db", "merge_new_glimmon_to_db", "compute_fingerprints"}:
+        from . import update
+        return getattr(update, name)
+    raise AttributeError(f"module 'glimmondb' has no attribute '{name}'")
